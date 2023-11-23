17fad0b0fbf232a8f7b43cf092ffdb1687d21c34
Julio Cesar <50207867+juliorovesta@users.noreply.github.com>
2023-11-23 18:40:54 -0300

    feat: basic proccess comunication

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('Hello world NodeJS!')
+import { spawn } from "child_process";
+
+const child = spawn("python", ["./src/python/main.py"]);
+child.on("exit", (code) => console.log("exitCode:", code));
+
+child.stdout.on("data", function (buffer) {
+    const response = JSON.parse(buffer);
+    console.log(response);
+});
+
+child.stdin.write(JSON.stringify({ args: [2, 3] }) + "\n");
+
+// import { PythonShell } from "python-shell";
+
+// PythonShell.runString("x=1+1;print(x)").then((messages) => {
+//     console.log("finished");
+// });
+
+// import { PythonShell } from "python-shell";
+
+// const result = await PythonShell.runString("x=1+1;print(x)");
+// console.log("finished", result);
--- src/python/main.py
+++ src/python/main.py
@@ -1,1 +1,24 @@
-print("Hello world Python!")
+from sys import stdin, stdout
+import json
+
+
+def minha_rede_neural(valor1, valor2):
+    return (valor1 * 2) * valor2
+
+
+while True:
+    message = stdin.readline()
+    message = json.loads(message)
+    args = message['args']
+
+    response = {}
+
+    try:
+        result = minha_rede_neural(*args)
+        response['data'] = result
+    except Exception as e:
+        response['error'] = str(e)
+
+    response = json.dumps(response)
+    stdout.write(response)
+    stdout.flush()
