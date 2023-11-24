a60f1e5df66f73d6c25d324e067da8fe339c9324
Julio Cesar <50207867+juliorovesta@users.noreply.github.com>
2023-11-24 18:54:00 -0300

    feat: execution with return async await

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,31 @@ const pyshell = new PythonShell('./src/python/main.py', {
     // scriptPath: './src/python',
     // args: []
 })
-pyshell.stdout.on('data', (data) => {
-    console.log(data)
-})
 
-pyshell
-    .send({ action: 'addition', args: [2, 3] })
-    .send({ action: 'subtraction', args: [10, 2] })
-    .send({ action: 'multiplication', args: [3, 5] })
-    .send({ action: 'division', args: [15, 3] })
+const sendCommand = (command: string | Object) =>
+    new Promise((resolve: (data: Object) => void) =>
+        pyshell.send(command).stdout.once('data', resolve)
+    )
+
+let result = await sendCommand({ action: 'addition', args: [2, 3] })
+console.log(result)
+
+result = await sendCommand({ action: 'subtraction', args: [10, 2] })
+console.log(result)
+
+// pyshell.stdout.on('data', (data) => {
+//     console.log(data)
+// })
+
+// pyshell
+//     .send({ action: 'addition', args: [2, 3] })
+//     .send({ action: 'subtraction', args: [10, 2] })
+//     .send({ action: 'multiplication', args: [3, 5] })
+//     .send({ action: 'division', args: [15, 3] })
+
+await setTimeout(2000)
+
+pyshell.kill('SIGQUIT')
 
 pyshell.end((err, code, signal) => {
     if (err) throw err
@@ -26,7 +42,3 @@ pyshell.end((err, code, signal) => {
     console.log('The exit signal was: ' + signal)
     console.log('# Finishing node app...')
 })
-
-await setTimeout(2000)
-
-pyshell.kill('SIGQUIT')
--- src/python/main.py
+++ src/python/main.py
@@ -20,36 +20,37 @@ class App:
     [staticmethod]
 
     def run():
-        while True:
-            for line in stdin:
-                response = {}
+        for line in stdin:
+            response = {}
 
-                try:
-                    message = json.loads(line)
-                    action = message["action"]
-                    args = message["args"]
-                    response["action"] = action
+            try:
+                message = json.loads(line)
+                action = message["action"]
+                args = message["args"]
+                # response["id"] = message["id"]
+                response["action"] = action
 
-                    calculator = Calculator()
-                    match action:
-                        case "addition":
-                            result = calculator.addition(*args)
-                        case "subtraction":
-                            result = calculator.subtraction(*args)
-                        case "multiplication":
-                            result = calculator.multiplication(*args)
-                        case "division":
-                            result = calculator.division(*args)
-                        case _:
-                            raise Exception("Invalid action!")
+                calculator = Calculator()
+                match action:
+                    case "addition":
+                        result = calculator.addition(*args)
+                    case "subtraction":
+                        result = calculator.subtraction(*args)
+                    case "multiplication":
+                        result = calculator.multiplication(*args)
+                    case "division":
+                        result = calculator.division(*args)
+                    case _:
+                        raise Exception("Invalid action!")
 
-                    response["result"] = result
-                except Exception as e:
-                    response["error"] = f"{e}"
+                response["result"] = result
+            except Exception as e:
+                response["error"] = f"{e}"
+                raise e
 
-                response = json.dumps(response)
-                stdout.write(f"{response} \n")
-                stdout.flush()
+            response = json.dumps(response)
+            stdout.write(f"{response} \n")
+            stdout.flush()
 
 
 App.run()
